4a85406787e1d79d30484bc2d8dbc5f4761c467e
Tigashov Philipp <tigashov.filipp@yandex.ru>
2022-04-18 21:02:28 +0300

    Begin code level 8

---
+++ src/main/java/com/example/networkchat/controllers/AuthController.java
@@ -0,0 +1,16 @@
+package com.example.networkchat.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AuthController {
+    @FXML
+    private Button enterButton;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private TextField passwordField;
+}
